15b5e8a73dd1afb70f21886f9ee746fe9d223fb0
Nic Scott <nicholaslscott@gmail.com>
2019-12-31 14:41:17 +1300

    Spicing up the app, adding some routes that simulate load by being 'slow'. Also a route that will error with a 503 And a status url (and pointing alb to use /status instead of /)

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 # app.py - a minimal flask api using flask_restful
-from flask import Flask
+from flask import Flask, abort, Response
 from flask_restful import Resource, Api
+from time import sleep
+from os import environ
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,6 +10,29 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@app.route("/slow")
+def SlowHelloWorld():
+    sleep(1)
+    return {'slowello': 'world: 1'}
+
+@app.route("/slow/<int:param>")
+def VariableSlowHelloWorld(param):
+    sleep(param)
+    return {'slowello': f'world: {param}'}
+
+@app.route("/oh_no")
+def fail():
+    #abort(503)
+    abort(503, description="Well this is embarrasing...")
+
+@app.route("/status")
+def status():
+    return {'status': 'OK',
+            'hostname': environ['HOSTNAME'],
+            'python': environ['PYTHON_VERSION'],
+            'region': environ['AWS_REGION']
+            }
 
 api.add_resource(HelloWorld, '/')
 
