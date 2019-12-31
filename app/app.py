682a0e02935fee418c800236f0c8f118b5364d0e
kband <nicholaslscott@gmail.com>
2020-01-01 12:36:44 +1300

    Nic/monitoring (#5)
    * SNS topic to send email to... myself
    Creating the most important alarm of all, billing.
    
    * Spicing up the app, adding some routes that simulate load by being 'slow'.
    Also a route that will error with a 503
    And a status url (and pointing alb to use /status instead of /)
    
    * Adding a bunch of cloudwatch alarms:
    alert if log events drops below 3
    alert if number of running tasks drops below 1
    anomaly detection on requests into the ALB, alert if it differs by more than 2 std
    alert if more than 5 503's are triggered in a 15minute period
    alert if average task cpu usage is greater than 50%
    alert if average task memory usage is greater than 75%
    
    * Whoops, had this backwards - want it to alarm if we get more than 5, not less
    
    * this demo project is getting expensive... boosting spending alarm to $10
    
    * spelling fix
    
    * Figured out how to have just the one function with a default parameter for the 'slow' route
    Added an app version
    Added some comments for the new routes
    Fixed bug where 'AWS_REGION' environment var doesn't exist locally, set a default value for the local case

--- app/app.py
+++ app/app.py
@@ -1,13 +1,40 @@
 # app.py - a minimal flask api using flask_restful
-from flask import Flask
+from flask import Flask, abort, Response
 from flask_restful import Resource, Api
+from time import sleep
+from os import environ
 
 app = Flask(__name__)
 api = Api(app)
 
+APP_VERSION = "20.01.001"
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+# A 'slow' route that takes 1s to return, or if an int is supplied, will return in int many seconds
+# Simulates waiting on a request to another microservice
+@app.route("/slow", defaults={'param': 1})
+@app.route("/slow/<int:param>")
+def VariableSlowHelloWorld(param):
+    sleep(param)
+    return {'slowello': f'world: {param}'}
+
+# route that throws a 503 error, apps have been known to do this unintentionally from time to time
+@app.route("/oh_no")
+def fail():
+    abort(503, description="Well this is embarrassing...")
+
+# status route for ALB health checks, including debugging data from container
+@app.route("/status")
+def status():
+    return {'status': 'OK',
+            'hostname': environ['HOSTNAME'],
+            'python': environ['PYTHON_VERSION'],
+            'region': environ.get('AWS_REGION', 'non-aws'),
+            'app_version': APP_VERSION
+            }
 
 api.add_resource(HelloWorld, '/')
 
