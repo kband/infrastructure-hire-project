72978eaef043ae44705ee8726ec4cdaf2eaf461f
Nic Scott <nicholaslscott@gmail.com>
2020-01-01 12:34:17 +1300

    Figured out how to have just the one function with a default parameter for the 'slow' route Added an app version Added some comments for the new routes Fixed bug where 'AWS_REGION' environment var doesn't exist locally, set a default value for the local case

--- app/app.py
+++ app/app.py
@@ -7,31 +7,33 @@ from os import environ
 app = Flask(__name__)
 api = Api(app)
 
+APP_VERSION = "20.01.001"
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@app.route("/slow")
-def SlowHelloWorld():
-    sleep(1)
-    return {'slowello': 'world: 1'}
-
+# A 'slow' route that takes 1s to return, or if an int is supplied, will return in int many seconds
+# Simulates waiting on a request to another microservice
+@app.route("/slow", defaults={'param': 1})
 @app.route("/slow/<int:param>")
 def VariableSlowHelloWorld(param):
     sleep(param)
     return {'slowello': f'world: {param}'}
 
+# route that throws a 503 error, apps have been known to do this unintentionally from time to time
 @app.route("/oh_no")
 def fail():
-    #abort(503)
     abort(503, description="Well this is embarrassing...")
 
+# status route for ALB health checks, including debugging data from container
 @app.route("/status")
 def status():
     return {'status': 'OK',
             'hostname': environ['HOSTNAME'],
             'python': environ['PYTHON_VERSION'],
-            'region': environ['AWS_REGION']
+            'region': environ.get('AWS_REGION', 'non-aws'),
+            'app_version': APP_VERSION
             }
 
 api.add_resource(HelloWorld, '/')
